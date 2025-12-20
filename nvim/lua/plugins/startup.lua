887ebf6cd072e1a58207a5fbe25d24f735142569
shafed <shaparenko.fedor@gmail.com>
2025-12-20 16:05:47 +0300

    snaks dashboard

---
+++ nvim/lua/plugins/startup.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    dashboard = { enabled = true },
+    -- Остальные модули по умолчанию отключены, если не указать enabled = true
+  },
+}

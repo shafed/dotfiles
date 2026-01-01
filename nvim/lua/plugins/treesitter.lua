1b175afef45bf9f11e2430994a6d46cabdf48834
shafed <shaparenko.fedor@gmail.com>
2026-01-01 14:24:24 +0300

    add .configs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter").setup({
+    require("nvim-treesitter.configs").setup({
       ensure_installed = { "python", "lua", "markdown", "markdown_inline", "cpp" },
       highlight = { enable = true, additional_vim_regex_highlighting = false },
       indent = { enable = true },
     })
   end
 }
-

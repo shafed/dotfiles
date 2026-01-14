665102c9948dc6e24e5aace3fa69af7e938bc666
shafed <shaparenko.fedor@gmail.com>
2026-01-14 11:08:45 +0000

    add table-tidy

---
+++ nvim/lua/plugins/md-table-tidy.lua
@@ -0,0 +1,7 @@
+return { "timantipov/md-table-tidy.nvim",
+    -- default config
+    opts = {
+      padding = 1,        -- number of spaces for cell padding
+      key = "<leader>tt", -- key for command :TableTidy<CR>
+    }
+}

6371ca8c25757404d8012a70e2a20365e47d7c6b
shafed <shaparenko.fedor@gmail.com>
2026-01-30 18:10:26 +0300

    add prettier and stylua

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,15 +1,16 @@
 return {
-  "stevearc/conform.nvim",
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        python = { "ruff_format", "ruff_organize_imports" },
-      },
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
-    })
-    vim.keymap.set('n', '<leader>f', function()
-      require("conform").format()
-    end, { desc = "Format code" })
-  end
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				python = { "ruff_format", "ruff_organize_imports" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+			},
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		})
+		vim.keymap.set("n", "<leader>f", function()
+			require("conform").format()
+		end, { desc = "Format code" })
+	end,
 }
-

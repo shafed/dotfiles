fc75b0d3a409184fc5368a47807b084d4631c5cc
shafed <shaparenko.fedor@gmail.com>
2026-01-30 18:11:09 +0300

    stylua

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,19 +7,22 @@ require("core.autocmds")
 -- === lazy.nvim bootstrap ===
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- === Плагины: автоподхват всех спецификаций из lua/plugins/*.lua ===
 require("lazy").setup({
-  { import = "plugins" },  -- импортирует все файлы в lua/plugins/
+	{ import = "plugins" }, -- импортирует все файлы в lua/plugins/
 }, {
-  change_detection = { notify = false },
+	change_detection = { notify = false },
 })
 
 -- === Цветовая схема (после загрузки плагинов) ===

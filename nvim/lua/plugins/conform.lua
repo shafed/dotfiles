d7e6e113c9ce6d26e0eb62c083bddb72c6f74b90
shafed <shaparenko.fedor@gmail.com>
2026-01-30 18:14:16 +0300

    add clang-format

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,6 +6,7 @@ return {
 				python = { "ruff_format", "ruff_organize_imports" },
 				markdown = { "prettier" },
 				lua = { "stylua" },
+				cpp = { "clang-format" },
 			},
 			format_on_save = { timeout_ms = 500, lsp_fallback = true },
 		})

f3689fb8e96f0e2d61a98960f4cada128423c620
shafed <shaparenko.fedor@gmail.com>
2026-02-01 09:06:08 +0300

    add virt-column

---
+++ nvim/lua/plugins/virt-column.lua
@@ -0,0 +1,19 @@
+-- https://github.com/lukas-reineke/virt-column.nvim
+-- Plugin that shows the veritcal bar or vertical column
+-- It replaces my vim.opt.colorcolumn = "80" configuration
+
+return {
+	"lukas-reineke/virt-column.nvim",
+	opts = {
+		-- char = "|",
+		-- char = "",
+		-- char = "┇",
+		-- char = "∶",
+		-- char = "∷",
+		-- char = "║",
+		-- char = "⋮",
+		-- char = "",
+		char = "󰮾",
+		virtcolumn = "80",
+	},
+}

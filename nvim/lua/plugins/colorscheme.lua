59d8de96281bfc50f81f33cecd5963bb3ed82ccb
shafed <shaparenko.fedor@gmail.com>
2026-02-05 15:11:36 +0300

    added bold, italic, inline_code, links highlights

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,19 @@
 return {
   {
     "sainnhe/gruvbox-material",
+    config = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          vim.api.nvim_set_hl(0, "@markup.strong.markdown_inline", { fg = "#e78a4e", bold = true }) -- Orange для жирного
+          vim.api.nvim_set_hl(0, "@markup.italic.markdown_inline", { italic = true, fg = "#a9b665" }) -- Green для курсива
+          vim.api.nvim_set_hl(0, "@lsp.type.decorator.markdown", { fg = "#d3869b", italic = true }) -- Purple для URL
+          vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { fg = "#d8a657" }) -- Yellow для инлайн кода
+        end,
+      })
+    end,
   },
 
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {

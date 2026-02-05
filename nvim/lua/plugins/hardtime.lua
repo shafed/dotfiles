4e403a67f96739df095de6fb969ea96ea77ceaf6
shafed <shaparenko.fedor@gmail.com>
2026-02-05 08:40:31 +0300

    nothing change

--- nvim/lua/plugins/hardtime.lua
+++ nvim/lua/plugins/hardtime.lua
@@ -1,14 +1,14 @@
 return {
-	"m4xshen/hardtime.nvim",
-	lazy = false,
-	dependencies = { "MunifTanjim/nui.nvim" },
-	opts = {},
-	opts = function(_, opts)
-		-- make sure the default table exists
-		opts.restricted_keys = opts.restricted_keys or {}
-		-- do NOT restrict gj / gk
-		opts.restricted_keys["gj"] = false
-		opts.restricted_keys["gk"] = false
-		opts.max_count = 12
-	end,
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {},
+  opts = function(_, opts)
+    -- make sure the default table exists
+    opts.restricted_keys = opts.restricted_keys or {}
+    -- do NOT restrict gj / gk
+    opts.restricted_keys["gj"] = false
+    opts.restricted_keys["gk"] = false
+    opts.max_count = 12
+  end,
 }

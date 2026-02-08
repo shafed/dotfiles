b583b78310110cf4c80acd28bbbc1c83e6be711f
shafed <shaparenko.fedor@gmail.com>
2026-02-08 09:35:02 +0300

    mini-files

---
+++ nvim/lua/plugins/mini-files.lua
@@ -0,0 +1,53 @@
+return {
+  "nvim-mini/mini.files",
+  opts = {
+    windows = {
+      preview = true,
+      width_focus = 30,
+      width_preview = 30,
+    },
+    options = {
+      -- Whether to use for editing directories
+      permanent_delete = false,
+      -- Disabled by default in LazyVim because neo-tree is used for that
+      use_as_default_explorer = true,
+    },
+    -- Module mappings created only inside explorer.
+    -- Use `''` (empty string) to not create one.
+    mappings = {
+      -- Default "q"
+      close = "<ESC>",
+      go_in = "l",
+      -- Default "L"
+      go_in_plus = "<CR>",
+      -- Default "h"
+      go_out = "H",
+      -- Default "H"
+      go_out_plus = "h",
+      mark_goto = "'",
+      mark_set = "m",
+      reset = "<BS>",
+      reveal_cwd = "@",
+      show_help = "g?",
+      synchronize = "=",
+      trim_left = "<",
+      trim_right = ">",
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (Directory of Current File)",
+    },
+    {
+      "<leader>E",
+      function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
+    },
+  },
+}

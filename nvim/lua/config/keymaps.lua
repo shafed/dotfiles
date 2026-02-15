c88a97be2ef1b0642d047876c86713792c866e87
shafed <shaparenko.fedor@gmail.com>
2026-02-15 08:50:07 +0300

    removed kj = esc

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,8 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "kj", "<ESC>", { desc = "[P]Exit insert mode with kj" })
-
 vim.keymap.set({ "n", "v" }, "H", "^", { desc = "[P]Go to the beginning line" })
 vim.keymap.set({ "n", "v" }, "L", "$", { desc = "[P]go to the end of the line" })
 

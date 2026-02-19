57b2d7baabd4f0a5722a678d0a70e39d309a6d28
shafed <shaparenko.fedor@gmail.com>
2026-02-19 12:07:27 +0300

    switch to blue inline code

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,10 +5,10 @@ return {
       vim.api.nvim_create_autocmd("ColorScheme", {
         pattern = "*",
         callback = function()
-          vim.api.nvim_set_hl(0, "@markup.strong.markdown_inline", { fg = "#e78a4e", bold = true }) -- Orange для жирного
+          vim.api.nvim_set_hl(0, "@markup.strong.markdown_inline", { fg = "#E78A4E", bold = true }) -- Orange для жирного
           vim.api.nvim_set_hl(0, "@markup.italic.markdown_inline", { italic = true, fg = "#a9b665" }) -- Green для курсива
-          vim.api.nvim_set_hl(0, "@lsp.type.decorator.markdown", { fg = "#d3869b", italic = true }) -- Purple для URL
-          vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { fg = "#d8a657" }) -- Yellow для инлайн кода
+          vim.api.nvim_set_hl(0, "@lsp.type.decorator.markdown", { fg = "#D3869B", italic = true }) -- Purple для URL
+          vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { fg = "#7DAEA3" }) -- Yellow для инлайн кода
         end,
       })
     end,

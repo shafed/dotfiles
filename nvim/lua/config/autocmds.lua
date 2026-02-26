c0a05a97f5e471aa59706865ecd2cc8be2361788
shafed <shaparenko.fedor@gmail.com>
2026-02-26 17:17:18 +0300

    added folded heading

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,8 +1,27 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- When I open markdown files I want to fold the markdown headings
+-- Originally I thought about using it only for skitty-notes, but I think I want
+-- it in all markdown files
 --
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- if vim.g.neovim_mode == "skitty" then
+vim.api.nvim_create_autocmd("BufRead", {
+  pattern = "*.md",
+  callback = function()
+    -- Get the full path of the current file
+    local file_path = vim.fn.expand("%:p")
+    -- Ignore files in my daily note directory
+    if file_path:match(os.getenv("HOME") .. "/github/obsidian_main/250%-daily/") then
+      return
+    end -- Avoid running zk multiple times for the same buffer
+    if vim.b.zk_executed then
+      return
+    end
+    vim.b.zk_executed = true -- Mark as executed
+    -- Use `vim.defer_fn` to add a slight delay before executing `zk`
+    vim.defer_fn(function()
+      vim.cmd("normal zk")
+      -- This write was disabling my inlay hints
+      -- vim.cmd("silent write")
+      vim.notify("Folded keymaps", vim.log.levels.INFO)
+    end, 100) -- Delay in milliseconds (100ms should be enough)
+  end,
+})

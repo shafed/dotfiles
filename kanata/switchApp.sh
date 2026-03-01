81e667423d50bc653c5ec181d2bb206616428aea
shafed <shaparenko.fedor@gmail.com>
2026-03-01 19:16:39 +0300

    eval for sioyek

--- kanata/switchApp.sh
+++ kanata/switchApp.sh
@@ -3,11 +3,8 @@
 APP_CLASS="$1"
 APP_COMMAND="$2"
 
-# Проверяем, запущено ли приложение
-if hyprctl clients | grep "class: $APP_CLASS"; then
-  # Если запущено - фокусируемся на нем
+if hyprctl clients | grep -q "class: $APP_CLASS"; then
   hyprctl dispatch focuswindow "class:$APP_CLASS"
 else
-  # Если не запущено - запускаем
-  $APP_COMMAND &
+  eval "$APP_COMMAND" &
 fi

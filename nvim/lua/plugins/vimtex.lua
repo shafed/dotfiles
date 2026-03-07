ee1826af9bf3dbe91d5af28971b8804b2cddbfd3
shafed <shaparenko.fedor@gmail.com>
2026-03-07 14:53:58 +0300

    sioyek + lualatex

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -4,6 +4,7 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "sioyek"
+    vim.g.vimtex_compiler_latexmk_engines = { _ = "-lualatex" }
   end,
 }

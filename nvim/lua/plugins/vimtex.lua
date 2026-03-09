5657a842b6a7480c30e4d68f9062456ab4e6154f
shafed <shaparenko.fedor@gmail.com>
2026-03-09 17:06:07 +0300

    function to copy .pdf from build out_dir

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,10 +1,22 @@
 return {
   "lervag/vimtex",
-  lazy = false, -- we don't want to lazy load VimTeX
-  -- tag = "v2.15", -- uncomment to pin to a specific release
+  lazy = false,
   init = function()
-    -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = "sioyek"
     vim.g.vimtex_compiler_latexmk_engines = { _ = "-lualatex" }
+    vim.g.vimtex_compiler_latexmk = {
+      out_dir = "build",
+      callback = 1,
+    }
+
+    -- Copy .pdf from build out_dir
+    vim.api.nvim_create_augroup("vimtex_callbacks", { clear = true })
+    vim.api.nvim_create_autocmd("User", {
+      group = "vimtex_callbacks",
+      pattern = "VimtexEventCompileSuccess",
+      callback = function()
+        vim.fn.system("cp build/*.pdf .")
+      end,
+    })
   end,
 }

d2edd909592a50695aae9a17e09facfcffa0e642
shafed <shaparenko.fedor@gmail.com>
2026-03-18 20:41:36 +0300

    delete copy pdf function

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -6,17 +6,6 @@ return {
     vim.g.vimtex_compiler_latexmk_engines = { _ = "-lualatex" }
     vim.g.vimtex_compiler_latexmk = {
       out_dir = "build",
-      callback = 1,
     }
-
-    -- Copy .pdf from build out_dir
-    vim.api.nvim_create_augroup("vimtex_callbacks", { clear = true })
-    vim.api.nvim_create_autocmd("User", {
-      group = "vimtex_callbacks",
-      pattern = "VimtexEventCompileSuccess",
-      callback = function()
-        vim.fn.system("cp build/*.pdf .")
-      end,
-    })
   end,
 }

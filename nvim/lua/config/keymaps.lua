514e5f01fc87d158d9c911d5eabd7193aa5c6c8c
shafed <shaparenko.fedor@gmail.com>
2026-03-28 12:39:12 +0000

    don't copy visual to clipboard

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.keymap.set("x", "p", '"_dP') -- Don't copy visual in clipboard
 
 local wk = require("which-key")
 wk.add({ { mode = { "n" }, { "<leader>t", group = "[P]todo/todoist" }, { "<leader>l", group = "[P]Log" } } })

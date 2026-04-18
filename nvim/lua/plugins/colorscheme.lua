d401e599e5c09ce65cf1125df30b92dba290f38d
shafed <shaparenko.fedor@gmail.com>
2026-04-18 11:20:07 +0300

    comment link highlight

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,7 +7,7 @@ return {
         callback = function()
           vim.api.nvim_set_hl(0, "@markup.strong.markdown_inline", { fg = "#E78A4E", bold = true }) -- Orange для жирного
           vim.api.nvim_set_hl(0, "@markup.italic.markdown_inline", { italic = true, fg = "#a9b665" }) -- Green для курсива
-          vim.api.nvim_set_hl(0, "@lsp.type.decorator.markdown", { fg = "#D3869B", italic = true }) -- Purple для URL
+          -- vim.api.nvim_set_hl(0, "@lsp.type.decorator.markdown", { fg = "#D3869B", italic = true }) -- Purple для URL
           vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { fg = "#7DAEA3" }) -- Yellow для инлайн кода
         end,
       })

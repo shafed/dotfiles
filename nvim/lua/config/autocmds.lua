5f9bfa18d80437ef9db73200e313428482e71b54
shafed <shaparenko.fedor@gmail.com>
2026-05-22 07:11:32 +0300

    comment autofold headings

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -8,33 +8,33 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
--- When I open markdown files I want to fold the markdown headings
--- Originally I thought about using it only for skitty-notes, but I think I want
--- it in all markdown files
---
--- if vim.g.neovim_mode == "skitty" then
-vim.api.nvim_create_autocmd("BufRead", {
-  pattern = "*.md",
-  callback = function()
-    -- Get the full path of the current file
-    local file_path = vim.fn.expand("%:p")
-    -- Ignore files in my daily note directory
-    if file_path:match(os.getenv("HOME") .. "/github/obsidian_main/250%-daily/") then
-      return
-    end -- Avoid running zk multiple times for the same buffer
-    if vim.b.zk_executed then
-      return
-    end
-    vim.b.zk_executed = true -- Mark as executed
-    -- Use `vim.defer_fn` to add a slight delay before executing `zk`
-    vim.defer_fn(function()
-      vim.cmd("normal zk")
-      -- This write was disabling my inlay hints
-      -- vim.cmd("silent write")
-      vim.notify("Folded keymaps", vim.log.levels.INFO)
-    end, 100) -- Delay in milliseconds (100ms should be enough)
-  end,
-})
+-- -- When I open markdown files I want to fold the markdown headings
+-- -- Originally I thought about using it only for skitty-notes, but I think I want
+-- -- it in all markdown files
+-- --
+-- -- if vim.g.neovim_mode == "skitty" then
+-- vim.api.nvim_create_autocmd("BufRead", {
+--   pattern = "*.md",
+--   callback = function()
+--     -- Get the full path of the current file
+--     local file_path = vim.fn.expand("%:p")
+--     -- Ignore files in my daily note directory
+--     if file_path:match(os.getenv("HOME") .. "/github/obsidian_main/250%-daily/") then
+--       return
+--     end -- Avoid running zk multiple times for the same buffer
+--     if vim.b.zk_executed then
+--       return
+--     end
+--     vim.b.zk_executed = true -- Mark as executed
+--     -- Use `vim.defer_fn` to add a slight delay before executing `zk`
+--     vim.defer_fn(function()
+--       vim.cmd("normal zk")
+--       -- This write was disabling my inlay hints
+--       -- vim.cmd("silent write")
+--       vim.notify("Folded keymaps", vim.log.levels.INFO)
+--     end, 100) -- Delay in milliseconds (100ms should be enough)
+--   end,
+-- })
 
 -- Mini.files relative numbers
 local function set_minifiles_numbers(args)
@@ -121,4 +121,21 @@ end
 vim.api.nvim_create_autocmd("VimEnter", {
   desc = "Auto pull Obsidian Vault on startup",
   callback = pull_obsidian_vault,
+})
+
+-- в init.lua
+vim.opt.viewoptions = "folds,cursor" -- сохраняем фолды и позицию курсора
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  pattern = "*.md",
+  callback = function()
+    vim.cmd("mkview")
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*.md",
+  callback = function()
+    vim.cmd("silent! loadview")
+  end,
 })
